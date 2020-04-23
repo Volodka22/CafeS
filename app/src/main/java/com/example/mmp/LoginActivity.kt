8ba9732d98963a53cc66deb1f2cb1e00952483ff
volodka22 <smagliy.vova@mail.ru>
2020-04-23 23:05:38 +0200

    add map

--- app/src/main/java/com/example/mmp/LoginActivity.kt
+++ app/src/main/java/com/example/mmp/LoginActivity.kt
@@ -1,80 +1,106 @@
 package com.example.mmp
 
-import android.app.Activity
+
 import android.content.Intent
 import android.content.pm.ActivityInfo
+import android.location.Geocoder
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.AuthUI.IdpConfig.EmailBuilder
-import com.firebase.ui.auth.AuthUI.IdpConfig.GoogleBuilder
-import com.firebase.ui.auth.ErrorCodes
-import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
-class LoginActivity : AppCompatActivity() {
+
+class LoginActivity : FragmentActivity(), OnMapReadyCallback {
 
     // Request code for sign in
     private val RC_SIGN_IN = 123
 
+    lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
 
-        requestedOrientation =  (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestedOrientation = (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val auth = FirebaseAuth.getInstance()
-        if (auth.currentUser != null) {
+        if (auth.currentUser == null) {
             // Already signed in
-            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            finish()
-        } else {
-            // Not signed in. Start the login flow.
-            startActivityForResult(
+            startActivity(
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
                     .setAvailableProviders(
                         listOf(
-                            GoogleBuilder().build()
+                            AuthUI.IdpConfig.GoogleBuilder().build()
                         )
                     )
                     .setTheme(R.style.AppTheme)
                     .setIsSmartLockEnabled(false)
-                    .build(),
-                RC_SIGN_IN
+                    .build()
             )
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
 
-        // RC_SIGN_IN is the request code you passed when starting the sign in flow.
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                finish()
-                return
-            } else {
-                // Sign in failed
-                when {
-                    response == null -> {
-                        // User pressed back button
-                        Log.e("Login", "Login canceled by User")
-                    }
-                    response.error!!.errorCode == ErrorCodes.NO_NETWORK -> {
-                        Log.e("Login", "No Internet Connection")
-                    }
-                    response.error!!.errorCode == ErrorCodes.UNKNOWN_ERROR -> {
-                        Log.e("Login", "Unknown Error")
-                    }
-                }
-            }
-            Log.e("Login", "Unknown sign in response")
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        mMap.addMarker(getLocationFromAddress("ул. Ялтинская, 81А, Калининград, Калининградская область")?.let {
+            MarkerOptions().position(
+                it
+            ).title("Ночной клуб завод")
+        })
+        
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(getLocationFromAddress("Россия Калининград Кафедральный собор"),
+            12F
+        ))
+        mMap.setOnMarkerClickListener {
             startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            finish()
+            true
+        }
+
+
+    }
+
+
+    private fun getLocationFromAddress(strAddress: String): LatLng? {
+
+        val aLocale: Locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Locale.Builder().setLanguage("RU").setScript("Latn").setRegion("RS").build()
+        } else {
+            Locale("RU")
+        }
+
+        val coder = Geocoder(this, aLocale)
+
+
+        return try {
+            val address = coder.getFromLocationName(strAddress, 1)
+            val location = address[0];
+
+            val p1 = LatLng(location.latitude, location.longitude);
+
+            p1
+        } catch (e: Exception) {
+
+            null
         }
     }
+
+
 }
